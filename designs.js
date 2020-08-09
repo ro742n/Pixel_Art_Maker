a10048e2841c12061ff1a102ea158c64311cab45
ro742n <69171317+ro742n@users.noreply.github.com>
2020-08-09 14:56:21 -0500

    Add files via upload

--- designs.js
+++ designs.js
@@ -10,8 +10,8 @@ var size = document.getElementById('sizePicker');
 var gridHeight = document.getElementById('inputHeight');
 var gridWidth = document.getElementById('inputWidth');
 var canvas = document.getElementById('pixelCanvas');
-var row = document.getElementById('row'); //In the starter code there are no elements to this ID. It's not declared on HTML code so it's doing nothing with them.
-var cell = document.getElementById('cell'); // same as row above
+var row = document.getElementById('row');
+var cell = document.getElementById('cell');
 var submit = document.createElement('button'); //Is the right way to declare submit variable and will this fix the event listerner piece below?
 // When size is submitted by the user, call makeGrid()
 // Not sure if the code below calls makeGrid or if I need to add something else here. Was told to close the brackets within {}
@@ -19,7 +19,6 @@ var submit = document.createElement('button'); //Is the right way to declare sub
 function makeGrid(gridHeight, gridWidth) {
   var table = document.getElementById('pixelCanvas');
   //table.innerHTML = ''; (Not sure if I need this.)
-  //Haven't asked mentor about the event listener function.
   submit.addEventListener('click', makeGrid);
 //For loop to create a row.
   for (var gridHeight = 0; gridHeight < 1; gridHeight++) {
